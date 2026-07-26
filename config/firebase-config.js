ca03465d5908cdd899239ea00b94a179e3245cfd
p-mn19 <poornimamen2019@gmail.com>
2026-07-26 18:03:22 +0530

    commit

---
+++ config/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHUwMoxok8U08XCwfe3i8_Zzw_pb_sCHs",
+  authDomain: "snapshot-4efae.firebaseapp.com",
+  projectId: "snapshot-4efae",
+  storageBucket: "snapshot-4efae.firebasestorage.app",
+  messagingSenderId: "385423659392",
+  appId: "1:385423659392:web:a0e00213d907712ee47165",
+  measurementId: "G-H0MJMZ52W7"
+};
+const OAUTH_CLIENT_ID = "385423659392-r1vf58up5c80kmk1fsa55bcjmoj8et2p.apps.googleusercontent.com"
